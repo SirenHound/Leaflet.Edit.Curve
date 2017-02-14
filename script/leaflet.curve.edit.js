6be6dbf9f743e5dede7869aee522c91a772b7f38
Ethan McCoy <SirenHound@users.noreply.github.com>
2017-02-14 16:54:34 +0800

    Update leaflet.curve.edit.js

--- script/leaflet.curve.edit.js
+++ script/leaflet.curve.edit.js
@@ -1,3 +1,10 @@
+L.DivIconStyled = L.DivIcon.extend({
+  createIcon: function(){
+    var r = L.DivIcon.prototype.createIcon.apply(this); L.Util.extend (r.style, this.options.styleOptions);//.backgroundColor = "yellow";
+    return r;
+  }
+});
+
 
 L.Curve.include({
   getLatLngs: function(){
@@ -6,5 +13,24 @@ L.Curve.include({
     }).map(function(coord){
       return L.latLng(coord);
     });
+  },
+  /** Create markers from the points defining this curve and assign them as types
+  */
+  getTypedMarkers: function(){
+    var markers = [];
+    var icons = {
+      qControl: new L.DivIconStyled({backgroundColor: 'yellow'})
+    };
+    
+    this._coords.forEach(function(coord, i, coords){
+      if ("string" === typeof coord){
+        switch (coord){
+          case "Q":// Quadratic has 2 coords
+          markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.qControl}));
+        }
+      }
+    }
+    return markers;
   }
+
 });
