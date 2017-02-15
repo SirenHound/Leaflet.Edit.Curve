83383daf0c02f7aa5ae89f783efcd7776a26cb0a
Ethan McCoy <ethan@antioch.org.au>
2017-02-15 14:14:12 +0800

    add node types (fragile)

--- script/leaflet.curve.edit.js
+++ script/leaflet.curve.edit.js
@@ -27,44 +27,54 @@ L.Curve.include({
       cControl2: new L.DivIconStyled({styleOptions:{backgroundColor: 'orange'}})
     };
     
+	var prev;
+	var layers = [];
+	var runSwitch = function(coord){
+		switch (coord){
+			case "M": case "L": // Single point
+				markers.push(new L.Marker(coords[i+1], {type: "anchor", icon: new L.DivIcon()}));
+				break;
+			case "V":
+				break;
+			case "H":
+				break;
+			case "Q":// Quadratic has 2 coords
+				markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.qControl}));
+				markers.push(new L.Marker(coords[i+2], {type: "anchor", icon: new L.DivIcon()}));
+				guiLayers.push(new L.Polyline([coords[i-1], coords[i+1]], {color: 'yellow'}));
+				guiLayers.push(new L.Polyline([coords[i+1], coords[i+2]], {color: 'yellow'}));
+				break;
+			case "C":// Cubic has 3 coords
+				markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.cControl1}));
+				markers.push(new L.Marker(coords[i+2], {type: "control1", icon: icons.cControl2}));
+				markers.push(new L.Marker(coords[i+3], {type: "anchor", icon: new L.DivIcon()}));
+				// first coord works with previous point. coords has already calculated point from partials (V, H)
+				guiLayers.push(new L.Polyline([coords[i-1], coords[i+1]], {color: 'yellow'}));
+				guiLayers.push(new L.Polyline([coords[i+2], coords[i+3]], {color: 'yellow'}));
+				break;
+			case "S":// S is shortcut C. Will fail following M L V H
+				var reflCoord = [2*coords[i-1][0] - coords[i-2][0], 2*coords[i-1][1] - coords[i-2][1]];
+				markers.push(new L.Marker(reflCoord, {type: "control1", icon: icons.cControl1}));
+				markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.cControl2}));
+				markers.push(new L.Marker(coords[i+2], {type: "anchor", icon: new L.DivIcon()}));
+				// first coord works with previous point. coords has already calculated point from partials (V, H)
+				guiLayers.push(new L.Polyline([coords[i-2], reflCoord], {color: 'yellow'}));
+				guiLayers.push(new L.Polyline([coords[i+2], coords[i+3]], {color: 'yellow'}));
+				break;
+		}
+		return [markers, guiLayers];
+	};
 	this._coords.forEach(function(coord, i, coords){
 		if ("string" === typeof coord){
-			switch (coord){
-				case "M": case "L": // Single point
-					markers.push(new L.Marker(coords[i+1], {type: "anchor", icon: new L.DivIcon()}));
-					break;
-				case "V":
-					break;
-				case "H":
-					break;
-				case "Q":// Quadratic has 2 coords
-					markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.qControl}));
-					markers.push(new L.Marker(coords[i+2], {type: "anchor", icon: new L.DivIcon()}));
-					guiLayers.push(new L.Polyline([coords[i-1], coords[i+1]], {color: 'yellow'}));
-					guiLayers.push(new L.Polyline([coords[i+1], coords[i+2]], {color: 'yellow'}));
-					break;
-				case "C":// Cubic has 3 coords
-					markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.cControl1}));
-					markers.push(new L.Marker(coords[i+2], {type: "control1", icon: icons.cControl2}));
-					markers.push(new L.Marker(coords[i+3], {type: "anchor", icon: new L.DivIcon()}));
-					// first coord works with previous point. coords has already calculated point from partials (V, H)
-					guiLayers.push(new L.Polyline([coords[i-1], coords[i+1]], {color: 'yellow'}));
-					guiLayers.push(new L.Polyline([coords[i+2], coords[i+3]], {color: 'yellow'}));
-					break;
-				case "S":// S is shortcut C. Will fail following M L V H
-					var reflCoord = [2*coords[i-1][0] - coords[i-2][0], 2*coords[i-1][1] - coords[i-2][1]];
-					markers.push(new L.Marker(reflCoord, {type: "control1", icon: icons.cControl1}));
-					markers.push(new L.Marker(coords[i+1], {type: "control1", icon: icons.cControl2}));
-					markers.push(new L.Marker(coords[i+2], {type: "anchor", icon: new L.DivIcon()}));
-					// first coord works with previous point. coords has already calculated point from partials (V, H)
-					guiLayers.push(new L.Polyline([coords[i-2], reflCoord], {color: 'yellow'}));
-					guiLayers.push(new L.Polyline([coords[i+2], coords[i+3]], {color: 'yellow'}));
-					break;
-				
-			}
+
+			layers.concat(runSwitch(coord));
+			prev = coord;
+		}
+		else{
+			layers.concat(runSwitch(prev));
 		}
 	});
-    return markers.concat(guiLayers);
+    return layers; //markers.concat(guiLayers);
   }
 
 });
