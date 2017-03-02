2a86960763edb15eb91ccd3d9328e4cfa9727f52
Ethan McCoy <ethan@antioch.org.au>
2017-03-02 09:49:46 +0800

    get rid of curve submodule

--- script/leaflet.draw.curve.js
+++ script/leaflet.draw.curve.js
@@ -24,43 +24,77 @@ Once Enabled,
 if (L.Draw) {
 
 L.Draw.Curve = L.Draw.Feature.extend({
+	Poly: L.Curve,
 	statics:{
 		SUPPORTED_TYPES: ["M", "L", "H", "V", "C", "Q", "S", "T"]
 	},
+	options: {
+		icon:new L.DivIcon({iconSize:new L.Point(8,8),className:"leaflet-div-icon leaflet-editing-icon"}),
+		allowIntersection: true
+	},
 	initialize: function(map, options){
 		L.Draw.Feature.prototype.initialize.call(this, map, options);
+		this._markerGroup = new L.LayerGroup().addTo(map);
+		this._markers = [];
+		
 		this.pointType = "M";
 	},
 	addHooks: function(){
 		L.Draw.Feature.prototype.addHooks.call(this);
+		this._poly=new this.Poly([],this.options.shapeOptions);
 		if (this._map) {
 			this._mapDraggable = this._map.dragging.enabled();
 			if (this._mapDraggable){ this._map.dragging.disable(); }
-			this._container.style.cursor="crosshair";
+			this._map._container.style.cursor="crosshair";
 			this._tooltip.updateContent({text:this._initialLabelText});
-			this._map.on("mousedown",this._onMouseDown,this).on("mousemove",this._onMouseMove,this);
+			this._map.on("mousedown",this._onMouseDown,this);//.on("mousemove",this._onMouseMove,this);
 		}
-		
-		//L.DomEvent.on(document, 'keydown', this._changePointType, this);
+		//Until interface is figured out
+		L.DomEvent.on(document, 'keydown', this._changePointType, this);
 	},
 	removeHooks: function(){
 		L.Draw.Feature.prototype.removeHooks.call(this);
 		
-		//L.DomEvent.off(document, 'keydown', this._changePointType, this);
+		//Until interface is figured out
+		L.DomEvent.off(document, 'keydown', this._changePointType, this);
 	},
 	_changePointType: function(evt){
 		var changeTo = evt.key.toUpperCase();
-		if (L.Draw.Curve.SUPPORTED_TYPES.contains(changeTo)){
+		if (L.Draw.Curve.SUPPORTED_TYPES.indexOf(changeTo) > -1 && this._markers.length){ // Must start with M
 			this.pointType = changeTo;
 		}
 		else{
-			console.warn("SVG point type '"+changeTo+"'is not supported");
+			console.warn("SVG point type '"+changeTo+"'is not supported or this is the first point and must not be changed from 'M'");
+		}
+		switch(changeTo){
+			case "V":
+				this._map._container.style.cursor="ns-resize";
+				break;
+			case "H":
+				this._map._container.style.cursor="ew-resize";
+				break;
+			default:
+				this._map._container.style.cursor="crosshair";
+				break;
 		}
 	},
+	// Will need to be a bit more versitile
+	_createMarker:function(t){
+		var e=new L.Marker(t, {icon: this.options.icon, zIndexOffset: 2*this.options.zIndexOffset});
+		this._markerGroup.addLayer(e);
+		return e;
+	},
+
 	_onMouseDown: function(evt) {
+
+		var e=evt.originalEvent;
+		this._mouseDownOrigin=L.point(e.clientX,e.clientY);
+
 		this._isDrawing = !0;
 		this._startLatLng = evt.latlng;
-		L.DomEvent.on(document, "mouseup", this._onMouseUp, this).preventDefault(evt.originalEvent);
+		//L.DomEvent.on(document, "mouseup", this._onMouseUp, this).preventDefault(evt.originalEvent);
+		this._map.on("mouseup", this._onMouseUp, this);
+		L.DomEvent.preventDefault(evt.originalEvent);
 	},
 	
 	/** Updates the shape being drawn using the current mouse position
@@ -79,9 +113,81 @@ L.Draw.Curve = L.Draw.Feature.extend({
 	// Finishing a point defined by a line that was dragged out (eg. C or Q)
 	// Taken from SimpleShape handler.
 	_onMouseUp: function(evt){
+		var orgEvt = evt.originalEvent;
+		if(this._mouseDownOrigin){ // SubClasses may define
+			var pxDist = L.point(orgEvt.clientX, orgEvt.clientY)
+				.distanceTo(this._mouseDownOrigin);
+				
+			if (Math.abs(pxDist)<9*(window.devicePixelRatio||1)){
+				this.addVertex(evt.latlng);
+			}
+		}
+		this._mouseDownOrigin=null;
+		
 		if (this._shape){ this._fireCreatedEvent(); }
 		//this.disable();
 		//if (this.options.repeatMode){ this.enable(); }
+	},
+	/** Adds a vertex (LatLng) to the object being drawn
+	* @param {L.LatLng} latLng - the LatLng to add to the curve in construction
+	*/
+	addVertex:function(latLng){
+		if (
+			this._markers.length > 0 &&
+			!this.options.allowIntersection &&
+			this._poly.newLatLngIntersects(latLng)
+		){
+			this._showErrorTooltip();
+		}
+		else{
+			if (this._errorShown) {
+				this._hideErrorTooltip();
+			}
+			latLng = this.pointType === "V"? L.latLng(latLng.lat, this._markers[this._markers.length-1].lng) : this.pointType === "V"? L.latLng(this._markers[this._markers.length-1].lat, latLng.lng) : latLng;
+			var newMarker = this._createMarker(latLng);
+			this._markers.push(newMarker);
+			
+			//add latlng to path
+			var path = this._poly.getPath();
+			//probably need to have a look at the path to see if current type matches last used type, see how many points need adding etc.
+			var instructions = path.filter(function(instr){return "string" === typeof instr;});
+			var lastInstr = instructions[instructions.length-1];
+			switch (this.pointType){
+				case "M": case "L": case "T": //one point?
+				/*	if (this.pointType !== lastInstr){ //bit sloppy, tighten up later
+						path.push(this.pointType, [latLng.lat, latLng.lng]);
+					}
+					else{
+						path.push([latLng.lat, latLng.lng]);
+					}
+				*/	break;
+				case "H": case "V": //one point, but really like, half a point
+					
+					break;
+				case "C": case "S":
+					break;
+				case "Q":
+					break;
+				
+				if (this.pointType !== "C"||false) // do a % thing on number of points
+				this._poly.setPath(path);
+			
+			}
+			
+				if (this.pointType !== lastInstr){ //bit sloppy, tighten up later
+						path.push(this.pointType, [latLng.lat, latLng.lng]);
+					}
+					else{
+						path.push([latLng.lat, latLng.lng]);
+					}
+				
+			
+			if (2===this._poly.getPath().length) {
+//				this._map.addLayer(this._poly);
+			}
+			//this._vertexChanged(latLng, true);
+			this.fire('vertexAdded', latLng);
+		}
 	},
 	
 });
