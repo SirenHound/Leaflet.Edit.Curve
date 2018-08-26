c82d73f4c29f783d63bdca812d9df30953cf3bb8
Sirenhound <ethan@antioch.org.au>
2018-08-26 09:55:45 +0800

    custom toolbars

--- script/leaflet.customtoolbar.js
+++ script/leaflet.customtoolbar.js
@@ -30,7 +30,7 @@ L.CustomToolbar = L.DrawToolbar.extend({
   getModeHandlers: function(map){
     return this._modeHandlers.map(function(modeHandler){
       // initialize handler from class
-      modeHandler.handler === modeHandler.handler || new modeHandler.Handler(map, modeHandler.enabled);
+      modeHandler.handler = modeHandler.handler || new modeHandler.Handler(map, modeHandler.enabled);
       delete modeHandler.Handler;
       return modeHandler;
     }, this);
@@ -43,5 +43,47 @@ L.CustomToolbar = L.DrawToolbar.extend({
       handler: handler,
       title: title
     });
-  }
+  },
+  // @method addToolbar(map): L.DomUtil
+  // Adds the toolbar to the map and returns the toolbar dom element
+  addToolbar: function (map) {
+    var container = L.DomUtil.create('div', 'leaflet-draw-section'),
+      buttonIndex = 0,
+      buttonClassPrefix = this._toolbarClass || '',
+      modeHandlers = this.getModeHandlers(map),
+      i;
+
+    this._toolbarContainer = L.DomUtil.create('div', 'leaflet-draw-toolbar leaflet-bar');
+    this._map = map;
+
+    for (i = 0; i < modeHandlers.length; i++) {
+      if (modeHandlers[i].enabled) {
+        this._initModeHandler(
+          modeHandlers[i].handler,
+          this._toolbarContainer,
+          buttonIndex++,
+          buttonClassPrefix,
+          modeHandlers[i].title
+        );
+      }
+    }
+
+    // if no buttons were added, do not add the toolbar
+    if (!buttonIndex) {
+      return;
+    }
+
+    // Save button index of the last button, -1 as we would have ++ after the last button
+    this._lastButtonIndex = --buttonIndex;
+
+    // Create empty actions part of the toolbar
+    this._actionsContainer = L.DomUtil.create('ul', 'leaflet-draw-actions');
+
+    // Add draw and cancel containers to the control container
+    container.appendChild(this._toolbarContainer);
+    container.appendChild(this._actionsContainer);
+
+    return container;
+  },
+
 });
